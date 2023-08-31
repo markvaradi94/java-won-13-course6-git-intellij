f508992dbeb1db99af1621c4fd8d894d42cef8a0
markvaradi94 <mark.varadi03@gmail.com>
2023-08-31 17:41:42 +0300

    changes on test branch

--- src/main/java/ro/fasttrackit/Main.java
+++ src/main/java/ro/fasttrackit/Main.java
@@ -3,5 +3,6 @@ package ro.fasttrackit;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Course 6 Preparation Example");
+        System.out.println("Added a new statement on test branch");
     }
 }
